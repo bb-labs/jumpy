64f0703bbede40c783c8c6c89af4838dc2d4d308
trumanpurnell <truman.purnell@gmail.com>
2020-01-30 00:03:25 -0800

    adds -e flag for linux container

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# aws_access_key_id = AKIA2OV7PPDHV2G7GWEF
+# aws_secret_access_key = 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
+
 mkdir -p ~/.aws
 
-echo "[default]\nregion = us-west-2\noutput = json" >~/.aws/config
-echo "[default]\naws_access_key_id = $1\naws_secret_access_key = $2" >~/.aws/credentials
+echo -e "[default]\nregion = us-west-2\noutput = json" >~/.aws/config
+echo -e "[default]\naws_access_key_id = $1\naws_secret_access_key = $2" >~/.aws/credentials
 
 # aws lambda create-function \
 #     --function-name numpy-endpoint-2 \
