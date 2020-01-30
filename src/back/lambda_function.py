65d16d43a709ce3dea821d6da98cdbba4441c67a
trumanpurnell <truman.purnell@gmail.com>
2020-01-30 01:40:15 -0800

    adds logging test to endpoint lambda

--- src/back/lambda_function.py
+++ src/back/lambda_function.py
@@ -6,59 +6,62 @@ DEBUG = False
 
 
 def lambda_handler(event, context):
+    gunit = np.random.rand(5, 5).tolist()
+
+    print(gunit)
     print(event)
     print(context)
 
     return {
-        'status': 200,
-        'body': np.random.rand(5, 5).tolist()
+        'statusCode': 200,
+        'body': gunit
     }
-    
-    # Each numpy call has arguments, a context, and a field
-    # args = json.loads(request.headers.get('args'))
-    # this = json.loads(request.headers.get('this'))
-    # field = json.loads(request.headers.get('field'))
 
-    # Lookup array arguments by their addresses
-    # for i, arg in enumerate(args):
-    #     if isinstance(arg, dict) and arg.get('address'):
-    #         args[i] = cache[arg['address']]
+# Each numpy call has arguments, a context, and a field
+# args = json.loads(request.headers.get('args'))
+# this = json.loads(request.headers.get('this'))
+# field = json.loads(request.headers.get('field'))
 
-    # Set the context depending on the `this` argument
-    # context = cache[this['address']] if isinstance(this, dict) else np
+# Lookup array arguments by their addresses
+# for i, arg in enumerate(args):
+#     if isinstance(arg, dict) and arg.get('address'):
+#         args[i] = cache[arg['address']]
 
-    # Get the numpy attribute of interest
-    # attribute = getattr(context, field)
+# Set the context depending on the `this` argument
+# context = cache[this['address']] if isinstance(this, dict) else np
 
-    # Call the numpy field if it's a method, otherwise just get its value
-    # result = attribute(*args) if callable(attribute) else attribute
+# Get the numpy attribute of interest
+# attribute = getattr(context, field)
 
-    # if DEBUG:
-    #     print('-------------{}--------------'.format(field))
-    #     print(this)
-    #     print(args)
-    #     print(result)
+# Call the numpy field if it's a method, otherwise just get its value
+# result = attribute(*args) if callable(attribute) else attribute
 
-    # If the result is of type np.array, cache it, and send the header back
-    # if isinstance(result, (np.ndarray, np.generic)):
-    #     address, _ = result.__array_interface__['data']
-    #     cache[address] = result
+# if DEBUG:
+#     print('-------------{}--------------'.format(field))
+#     print(this)
+#     print(args)
+#     print(result)
 
-    #     return make_response(json.dumps({
-    #         'address': address,
-    #         'size': result.size,
-    #         'shape': result.shape,
-    #         'dtype': str(result.dtype),
-    #         'strides': result.strides,
-    #     }), {
-    #         'content-type': "array"
-    #     })
+# If the result is of type np.array, cache it, and send the header back
+# if isinstance(result, (np.ndarray, np.generic)):
+#     address, _ = result.__array_interface__['data']
+#     cache[address] = result
 
-    # If the result is of type bytes, send it over.
-    # if isinstance(result, bytes):
-    #     return make_response(result, {
-    #         'content-type': "bytes",
-    #     })
+#     return make_response(json.dumps({
+#         'address': address,
+#         'size': result.size,
+#         'shape': result.shape,
+#         'dtype': str(result.dtype),
+#         'strides': result.strides,
+#     }), {
+#         'content-type': "array"
+#     })
 
-    # Otherwise just dump the json
-    # return json.dumps(result)
+# If the result is of type bytes, send it over.
+# if isinstance(result, bytes):
+#     return make_response(result, {
+#         'content-type': "bytes",
+#     })
+
+# Otherwise just dump the json
+# return json.dumps(result)
