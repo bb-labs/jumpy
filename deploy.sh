8220b58022c8ce42535d204215c267f8d8b5ca33
trumanpurnell <truman.purnell@gmail.com>
2020-01-30 00:00:36 -0800

    enables credential pass to docker container

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-zip -r ./package.zip .
+mkdir -p ~/.aws
 
-aws lambda create-function \
-    --function-name numpy-endpoint-2 \
-    --runtime python3.7 \
-    --zip-file fileb://package.zip \
-    --handler numpy-endpoint-2.lambda_handler \
-    --role arn:aws:iam::718734850255:role/service-role/admin
+echo "[default]\nregion = us-west-2\noutput = json" >~/.aws/config
+echo "[default]\naws_access_key_id = $1\naws_secret_access_key = $2" >~/.aws/credentials
+
+# aws lambda create-function \
+#     --function-name numpy-endpoint-2 \
+#     --runtime python3.7 \
+#     --zip-file fileb://package.zip \
+#     --handler numpy-endpoint-2.lambda_handler \
+#     --role arn:aws:iam::718734850255:role/service-role/admin
