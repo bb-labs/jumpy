407b6d420d1657c0d1da660d5251ef246af0f051
trumanpurnell <truman.purnell@gmail.com>
2020-01-29 23:15:36 -0800

    modifies deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+zip -r ./package.zip .
 
+aws lambda create-function \
+    --function-name numpy-endpoint-2 \
+    --runtime python3.7 \
+    --zip-file fileb://package.zip \
+    --handler numpy-endpoint-2.lambda_handler \
+    --role arn:aws:iam::718734850255:role/service-role/admin
