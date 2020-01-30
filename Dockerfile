9f17e8dddac865a3fe4a817dc55b3d22760d30ac
trumanpurnell <truman.purnell@gmail.com>
2020-01-30 01:17:11 -0800

    moves deploy script to src folder to appease aws

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,3 @@ FROM lambci/lambda:build-python3.7
 RUN git clone https://github.com/bb-labs/jumpy.git
 RUN pip install numpy
 RUN cp -r /var/lang/lib/python3.7/site-packages/numpy ./jumpy/src/back
-RUN zip -r ./package.zip ./jumpy/src/back/
--- src/back/deploy.sh
+++ src/back/deploy.sh
@@ -5,9 +5,11 @@ mkdir -p ~/.aws
 echo -e "[default]\nregion = us-west-2\noutput = json" >~/.aws/config
 echo -e "[default]\naws_access_key_id = $1\naws_secret_access_key = $2" >~/.aws/credentials
 
+zip -r package.zip .
+
 aws lambda create-function \
-    --function-name numpy-endpoint-2 \
+    --function-name tester23 \
     --runtime python3.7 \
-    --zip-file fileb://jumpy/package.zip \
-    --handler numpy-endpoint-2.lambda_handler \
+    --zip-file fileb://package.zip \
+    --handler lambda_function.lambda_handler \
     --role arn:aws:iam::718734850255:role/service-role/admin
