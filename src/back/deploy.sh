45738fed344e5dfda15a2f420346c7f56e619a8c
trumanpurnell <truman.purnell@gmail.com>
2020-01-30 19:54:03 -0800

    updates deploy script to overwrite lambda if already exists

--- src/back/deploy.sh
+++ src/back/deploy.sh
@@ -9,9 +9,20 @@ zip -r package.zip .
 
 ls -lah
 
-aws lambda create-function \
+aws lambda get-function \
     --function-name numpy-endpoint \
-    --runtime python3.7 \
-    --zip-file fileb://package.zip \
-    --handler lambda_function.lambda_handler \
-    --role arn:aws:iam::718734850255:role/service-role/admin
+    --output text \
+    &>response
+
+if grep -q "An error occurred" response; then
+    aws lambda create-function \
+        --function-name numpy-endpoint \
+        --runtime python3.7 \
+        --zip-file fileb://package.zip \
+        --handler lambda_function.lambda_handler \
+        --role arn:aws:iam::718734850255:role/service-role/admin
+else
+    aws lambda update-function-code \
+        --function-name numpy-endpoint \
+        --zip-file fileb://package.zip
+fi
